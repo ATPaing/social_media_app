940870ee2fe5fbdbf69395c99eee2ee4f36c2abb
ATPaing <aungthetpaing.2001@gmail.com>
2024-07-15 17:04:11 +0700

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const mysql = require('mysql2');
 
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'abc@123',
-    database: 'user_list'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 }); 
 
 db.connect((err) => {
